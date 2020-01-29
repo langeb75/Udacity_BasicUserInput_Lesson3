8fc61984ec7c094877c98bd2d544d4ac8b69e81a
Brad Lange <blange@gulllakecs.org>
2020-01-28 20:21:45 -0500

    Add a Global Variable for teamAScore  - Updated points 1, 2 & 3 Methods

--- app/src/main/java/com/example/lesson3/MainActivity.java
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -7,37 +7,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Declare a global variable for Team A Score
+    int teamAScore = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * Adds Three Points to Team A's Score
-     */
-    public void addThreePointsTeamA(View v) {
-        displayForTeamA(3);
-    }
-    /**
-     * Adds Two Points to Team A's Score
-     */
-    public void addTwoPointsTeamA(View view) {
-        displayForTeamA(2);
-    }
 
     /**
      * Adds One Point to Team A's Score
      */
     public void addOnePointTeamA(View view) {
-        displayForTeamA(1);
+        teamAScore = teamAScore + 1;
+        displayScoreForTeamA(teamAScore);
     }
 
+    /**
+     * Adds Two Points to Team A's Score
+     */
+    public void addTwoPointsTeamA(View view) {
+        teamAScore = teamAScore + 2;
+        displayScoreForTeamA(teamAScore);
+    }
+
+    /**
+     * Adds Three Points to Team A's Score
+     */
+    public void addThreePointsTeamA(View v) {
+        teamAScore = teamAScore + 3;
+        displayScoreForTeamA(teamAScore);
+    }
 
      /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
+    public void displayScoreForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
